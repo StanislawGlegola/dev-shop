19d08dc82e9e1cb344e18ecf25bdd5a759d9867d
Stanislaw <sta.glegola@gmail.com>
2019-09-07 11:52:27 +0200

    database update (added Category table with one id)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,5 @@ insert INTO Product (id, productName, category_id, discription, price, amount) v
 insert INTO Product (id, productName, category_id, discription, price, amount) values (nextval('product_seq'), 'Laptop Samsung',1, 'laptop5',2900,10);
 insert INTO Product (id, productName, category_id, discription, price, amount) values (nextval('product_seq'), 'Laptop Lenovo',1, 'laptop6',2600,10);
 insert INTO Product (id, productName, category_id, discription, price, amount) values (nextval('product_seq'), 'Laptop Acer',1, 'laptop7',4800,10);
+
+insert INTO Category(id, categoryName) values (1,laptop);
