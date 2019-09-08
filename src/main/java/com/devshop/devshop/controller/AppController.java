6351d875e72e4c5fcac2cae00e7aefa319173b9b
Stanislaw <sta.glegola@gmail.com>
2019-09-08 12:19:21 +0200

    cart viewer implementation added

--- src/main/java/com/devshop/devshop/controller/AppController.java
+++ src/main/java/com/devshop/devshop/controller/AppController.java
@@ -1,6 +1,7 @@
 package com.devshop.devshop.controller;
 
 import com.devshop.devshop.model.Category;
+import com.devshop.devshop.model.OrderItem;
 import com.devshop.devshop.service.DevshopService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +24,15 @@ public class AppController {
         List<Category>categories=devshopService.findAll();
         ModelAndView modelAndView=new ModelAndView("home");
         modelAndView.addObject("categories", categories);
+
+        return modelAndView;
+    }
+    @GetMapping("/cart")
+    public ModelAndView getCartScreen(){
+
+        List<OrderItem> orderedItems=devshopService.productsInCart();
+        ModelAndView modelAndView=new ModelAndView("cart");
+        modelAndView.addObject("orderItems",orderedItems);
 
         return modelAndView;
     }
--- src/main/java/com/devshop/devshop/service/DevshopService.java
+++ src/main/java/com/devshop/devshop/service/DevshopService.java
@@ -1,7 +1,10 @@
 package com.devshop.devshop.service;
 
 import com.devshop.devshop.model.Category;
+import com.devshop.devshop.model.OrderItem;
 import com.devshop.devshop.repository.CategoryRepository;
+import com.devshop.devshop.repository.OrderItemRepository;
+import com.devshop.devshop.repository.OrderRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,13 +12,21 @@ import java.util.List;
 @Service
 public class DevshopService {
     private final CategoryRepository categoryRepository;
+    private final OrderItemRepository orderItemRepository;
 
-    public DevshopService(CategoryRepository categoryRepository) {
+    public DevshopService(CategoryRepository categoryRepository, OrderRepository orderRepository, OrderItemRepository orderItemRepository, OrderItem orderItem) {
         this.categoryRepository = categoryRepository;
+        this.orderItemRepository = orderItemRepository;
+
     }
 
     public List<Category> findAll() {
         List<Category> allCategories = categoryRepository.findAll();
         return allCategories;
+    }
+
+    public List<OrderItem> productsInCart(){
+        List<OrderItem>orderedItems=orderItemRepository.findAll();
+        return orderedItems;
     }
 }
