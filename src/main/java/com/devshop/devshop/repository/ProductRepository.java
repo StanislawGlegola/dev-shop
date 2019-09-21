1a7086d3cf7a3f433f3e5f3e5ce163c13db83c07
Stanislaw <sta.glegola@gmail.com>
2019-09-21 10:12:27 +0200

    Update changes from review.

--- src/main/java/com/devshop/devshop/repository/ProductRepository.java
+++ src/main/java/com/devshop/devshop/repository/ProductRepository.java
@@ -2,11 +2,9 @@ package com.devshop.devshop.repository;
 
 import com.devshop.devshop.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     List<Product> findByCategoryId(Long id);
-
 }
